b2bbd26d9e46db486afe2813c0ee7804bba496f1
Daniel Parra <danse1434@hotmail.com>
2021-09-23 12:53:28 -0500

    Se recompila y actualiza informe final

--- reports/Libro/compilarLibroHTML.bat
+++ reports/Libro/compilarLibroHTML.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 
-RScript -e "bookdown::render_book(c('index.Rmd', 'L001_Introduccion.Rmd', 'L002_Objetivos.Rmd', 'L003_Justificacion.Rmd', 'L101_EstructuraFRE.Rmd', 'L102_RecetariosOficiales.Rmd', 'L103_Medicamentos.Rmd', 'L104_GestionInformes.Rmd', 'L301_Regional.Rmd', 'L500_Glosario.Rmd', 'L502_Creditos_2.Rmd'))"
+RScript -e "bookdown::render_book(c('index.Rmd', 'L001_Introduccion.Rmd', 'L002_Objetivos.Rmd', 'L003_Justificacion.Rmd', 'L101_EstructuraFRE.Rmd', 'L102_RecetariosOficiales.Rmd', 'L103_Medicamentos.Rmd', 'L104_GestionInformes.Rmd', 'L301_Regional.Rmd', 'L500_Glosario.Rmd', 'L501_Creditos.Rmd'))"
 
 PAUSE
