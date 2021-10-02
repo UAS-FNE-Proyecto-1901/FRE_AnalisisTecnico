e91980e23d26ab30e0027019a5e7d05fc33af0ab
Daniel Parra <danse1434@hotmail.com>
2021-10-02 15:37:56 -0500

    Actualización de Informe 2 de septiembre 2021 -- 2

--- reports/Libro/compilarLibroPDF.bat
+++ reports/Libro/compilarLibroPDF.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 
-RScript -e "bookdown::render_book(c('index.Rmd', 'L001_Introduccion.Rmd', 'L002_Objetivos.Rmd', 'L003_Justificacion.Rmd', 'L101_EstructuraFRE.Rmd', 'L102_RecOfi_existencias.Rmd', 'L103_RecOfi_costo.Rmd', 'L104_RecOfi_adquisicion.Rmd', 'L105_RecOfi_despacho.Rmd', 'L151_Medicamentos_Med.Rmd', 'L152_Medicamentos_Adq.Rmd', 'L153_Medicamentos_Rcp.Rmd', 'L154_Medicamentos_Alm.Rmd', 'L155_Medicamentos_Prec.Rmd', 'L201_GestionInformes.Rmd', 'L301_Regional.Rmd', 'L500_Glosario.Rmd', 'L501_Creditos.Rmd'), bookdown::pdf_book(keep_tex = TRUE, includes = rmarkdown::includes(in_header = c('estructura.tex') ), toc_depth=3 ) )"
+RScript -e "bookdown::render_book(c('index.Rmd', 'L001_Introduccion.Rmd', 'L002_Objetivos.Rmd', 'L003_Justificacion.Rmd', 'L101_EstructuraFRE.Rmd', 'L102_RecOfi_existencias.Rmd', 'L103_RecOfi_costo.Rmd', 'L104_RecOfi_adquisicion.Rmd', 'L105_RecOfi_despacho.Rmd', 'L151_Medicamentos_Med.Rmd', 'L152_Medicamentos_Adq.Rmd', 'L153_Medicamentos_Rcp.Rmd', 'L154_Medicamentos_Alm.Rmd', 'L155_Medicamentos_Prec.Rmd', 'L200_RutaTecnologica.Rmd', 'L301_GestionInformes.Rmd', 'L451_Regional.Rmd', 'L500_Glosario.Rmd', 'L501_Creditos.Rmd'), bookdown::pdf_book(keep_tex = TRUE, includes = rmarkdown::includes(in_header = c('estructura.tex') ), toc_depth=3 ) )"
 
 PAUSE
