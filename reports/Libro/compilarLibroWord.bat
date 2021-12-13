eda2babca5aca3db8ae1e48db955920901ac1f24
Daniel Parra <danse1434@hotmail.com>
2021-12-13 12:34:01 -0500

    Actualización de informe final con correcciones de JFVP

--- reports/Libro/compilarLibroWord.bat
+++ reports/Libro/compilarLibroWord.bat
@@ -1,5 +1,5 @@
 ECHO ON
 
-RScript -e "bookdown::render_book(c('index.Rmd'), bookdown::word_document2(), config_file='_wordbook.yml')"
+RScript -e "bookdown::render_book(c('index.Rmd'), bookdown::word_document2(), config_file='_wordbook.yml', quiet=TRUE)"
 
 PAUSE
